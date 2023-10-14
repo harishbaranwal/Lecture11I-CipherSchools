ac475515c119ee4458b3d8f70c53097852a284ca
harishbaranwal <114431658+harishbaranwal@users.noreply.github.com>
2023-10-14 10:44:38 +0530

    Create lecture11I.cpp

---
+++ lecture11I.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+
+    long long int a;
+    int b;
+
+    cout << "size of int is: " <<sizeof(b) << " the size of long long int is: " << sizeof(a) << endl;
+    
+    cout << "size of int is: " <<sizeof(int);
+
+    return 0;
+}
